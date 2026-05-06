357f59abdf728d6dd58cee858c20324150ff2932
Oscar Tadeo <oscarlanda8813@gmail.com>
2026-05-05 21:16:19 -0600

    Agregue mis postres

--- pequeño_cesar/src/pequeño_cesar/TadeoPostres/RefrigeradorPostres.java
+++ pequeño_cesar/src/pequeño_cesar/TadeoPostres/RefrigeradorPostres.java
@@ -1,5 +1,30 @@
 package pequeño_cesar.TadeoPostres;
 
-public class RefrigeradorPostres {
+import java.util.List;
+import java.util.ArrayList; 
 
+public class RefrigeradorPostres {
+    private List<Postres> listaPostres;
+
+    public RefrigeradorPostres() {
+        this.listaPostres = new ArrayList<>();
+       
+        listaPostres.add(new Postres("Flan", 25, 120.00));
+        
+     
+        listaPostres.add(new Postres("Pastel", 20, 150.00));
+        
+        
+        listaPostres.add(new Postres("Gelatina",  35, 130.00));
+    }
+
+	public List<Postres> getListaPostres() {
+		return listaPostres;
+	}
+
+	public void setListaBebidas(List<Postres> listaPostres) {
+		this.listaPostres = listaPostres;
+	}
+    
+    
 }
